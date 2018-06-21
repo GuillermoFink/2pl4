a9e2f451514196876699401906ad3d6b0d583561
Guillermo Fink (CM) <guillermo.fink@ar.vision101.corp>
2018-06-21 19:31:18 -0300

    servicios y clases

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes, Route } from '@angular/router';
+import { Validators, FormControl, FormGroup, FormBuilder } from '@angular/forms';
 
+// CLASES UTILIZADAS
+import { Usuario } from './clases/usuario';
+import { Mascota } from './clases/mascota';
+
+// SERVICIOS PROPIOS
+import { UsuarioService } from './servicios/usuario/usuario.service';
+import { MascotaService } from './servicios/mascota/mascota.service';
+import { MiHttpService } from './servicios/http/mi-http.service';
+import { HttpModule } from '@angular/http';
+
+// COMPONENTES
 import { AppComponent } from './app.component';
 import { LoginComponent } from './componentes/login/login.component';
 import { HomeComponent } from './componentes/home/home.component';
+
+
+const config: Routes = [
+  {
+    path: '',
+    component: LoginComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  }
+];
 
 @NgModule({
   declarations: [
@@ -12,9 +39,14 @@ import { HomeComponent } from './componentes/home/home.component';
     HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule,
+    RouterModule.forRoot(config)
   ],
-  providers: [],
+  providers: [Usuario, Mascota, MiHttpService, UsuarioService,MascotaService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/clases/mascota.ts
@@ -0,0 +1,8 @@
+export class Mascota {
+    id: number;
+    id_usuario: number;
+    raza: string;
+    color: string;
+    edad: number;
+    tipo: number;
+}
